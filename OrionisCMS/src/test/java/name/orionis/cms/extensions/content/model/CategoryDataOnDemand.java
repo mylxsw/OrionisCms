07bce01fa54a41ad0bb4f1c6150f76fc4dd26912
orionis <orionis@orionis-PC>
2013-05-13 00:47:54 +0800

    增加了文章内容，分类实体

---
+++ OrionisCMS/src/test/java/name/orionis/cms/extensions/content/model/CategoryDataOnDemand.java
@@ -0,0 +1,7 @@
+package name.orionis.cms.extensions.content.model;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Category.class)
+public class CategoryDataOnDemand {
+}
